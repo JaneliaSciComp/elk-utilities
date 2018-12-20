39a05bbe8b30742e71f5af9bf970761d0135d2a2
Rob Svirskas <svirskasr@janelia.hhmi.org>
2018-12-20 14:28:47 -0500

    Now gets day limit from config. Added CI.

--- bin/delete_elastic_indices.py
+++ bin/delete_elastic_indices.py
@@ -1,14 +1,15 @@
 import argparse
 from datetime import datetime, date, timedelta
-from elasticsearch import Elasticsearch
-import json
 import re
 import sys
 import colorlog
 import requests
+from elasticsearch import Elasticsearch
 
 # Configuration
 CONFIG = {'config': {'url': 'http://config.int.janelia.org/'}}
+POLICY = {}
+SERVER = {}
 
 # -----------------------------------------------------------------------------
 def call_responder(server, endpoint):
@@ -27,15 +28,19 @@ def call_responder(server, endpoint):
 
 def initialize_program():
     """ Initialize database """
-    global CONFIG
-    data = call_responder('config', 'config/servers')
+    global CONFIG, SERVER, POLICY
+    data = call_responder('config', 'config/rest_services')
     CONFIG = data['config']
+    data = call_responder('config', 'config/servers')
+    SERVER = data['config']
+    data = call_responder('config', 'config/retention_policies')
+    POLICY = data['config']
 
 
-def last_day_of_month(date):
-    if date.month == 12:
-        return date.replace(day=31)
-    return date.replace(month=date.month+1, day=1) - timedelta(days=1)
+def last_day_of_month(indate):
+    if indate.month == 12:
+        return indate.replace(day=31)
+    return indate.replace(month=indate.month+1, day=1) - timedelta(days=1)
 
 
 def process_indices():
@@ -43,30 +48,36 @@ def process_indices():
     deleted = ddeleted = 0
     today = date.today()
     try:
-        es = Elasticsearch(CONFIG['elk-elastic']['address'])
+        esearch = Elasticsearch(SERVER['elk-elastic']['address'])
     except Exception as ex:
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         print(message)
         sys.exit(-1)
-    health = es.cluster.health()
+    health = esearch.cluster.health()
     print("Cluster status:", health['status'])
-    for index in es.indices.get('*'):
-        if index[0] == '.' or not re.search('-20\d{2}\.\d{2}', index):
+    for index in esearch.indices.get('*'):
+        if index[0] == '.':
             continue
-        stats = es.indices.stats(index)
+        maxdays = 0
+        for policy in POLICY:
+            if re.search(POLICY[policy]['term'], index):
+                maxdays = POLICY[policy]['days']
+                logger.debug("%s met %s policy (%d days)" % (index, policy, maxdays))
+                break
+        if not maxdays:
+            continue
+        stats = esearch.indices.stats(index)
         docs = stats['indices'][index]['primaries']['docs']['count']
         found += 1
         dfound += docs
         field = index.split('-')
         indexdate = field[-1]
-        maxdays = 7
         if len(indexdate) == 7:
             # YY-MM : add the last day of that month
-            ym = indexdate.split('.')
-            ldom = last_day_of_month(date(int(ym[0]), int(ym[1]), 1))
+            yrmo = indexdate.split('.')
+            ldom = last_day_of_month(date(int(yrmo[0]), int(yrmo[1]), 1))
             indexdate += '.' + str(ldom.day)
-            maxdays = 60
         idateobj = datetime.strptime(indexdate, "%Y.%m.%d").date()
         delta = (today - idateobj).days
         logger.info("%s (%s docs): %d day(s)", index, "{:,}".format(docs), delta)
@@ -74,12 +85,13 @@ def process_indices():
             deleted += 1
             ddeleted += docs
             if ARG.DELETE:
-                es.indices.delete(index=index, ignore=[400, 404])
+                esearch.indices.delete(index=index, ignore=[400, 404])
                 logger.error("Deleted %s (%s docs)", index, "{:,}".format(docs))
             else:
                 logger.warning("Would have deleted %s (%s docs)", index, "{:,}".format(docs))
     print("Indices found: %d (%s docs)" % (found, "{:,}".format(dfound)))
-    print("Indices %sdeleted: %d (%s docs)" % ('' if (ARG.DELETE) else 'that would have been ', deleted, "{:,}".format(ddeleted)))
+    print("Indices %sdeleted: %d (%s docs)" % ('' if (ARG.DELETE) else \
+        'that would have been ', deleted, "{:,}".format(ddeleted)))
 
 
 # -----------------------------------------------------------------------------
